0f7c27b1a4d0939031b30c00c9b3da7355f0c31d
Misha Krit <misha.krit@bitgamelabs.com>
2017-01-01 16:26:40 +0200

    lesson 4

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 class StorePicker extends React.Component{
 	render(){
-		return (
-			<p>hello</p>
+		return ( //Возвращать только 1 узел и все внутри него
+			<form className="store-selector">
+				{/* комменты так */}
+				<h2>Please enater a Store</h2>
+				<input type="text" required placeholder="Store Name"/>
+				<button type="submit">Visit Store -></button>
+			</form>
 		);
 	}
 }
