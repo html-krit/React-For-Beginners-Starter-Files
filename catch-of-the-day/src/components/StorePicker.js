ff74077e2bec082a681854ae1ae1f6512d4b7fd3
Misha Krit <misha.krit@bitgamelabs.com>
2017-01-07 13:02:04 +0200

    lesson 11

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,12 +2,25 @@ import React from 'react';
 import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component{
+
+	//constructor(){
+	//	super();
+	//	this.goToStore = this.goToStore.bind(this);
+	//}
+
+	goToStore(event) {
+		event.preventDefault();
+		console.log(this.storeInput.value);
+	}
+
 	render(){
 		return ( //Возвращать только 1 узел и все внутри него
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={(e) => {this.goToStore(e)}}>
 				{/* комменты так */}
 				<h2>Please enater a Store</h2>
-				<input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+				<input type="text" required placeholder="Store Name"
+				       defaultValue={getFunName()}
+				ref={(input)=>{this.storeInput = input}}/>
 				<button type="submit">Visit Store -></button>
 			</form>
 		);
