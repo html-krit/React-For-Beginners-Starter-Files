fb3fee9b025fa7a5bbfd74585a4fd685dc4ba84f
Misha Krit <misha.krit@bitgamelabs.com>
2017-01-08 15:44:10 +0200

    lesson 14

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component{
 
@@ -9,6 +10,7 @@ class App extends React.Component{
 		super();
 
 		this.addFish = this.addFish.bind(this);
+		this.loadSamples = this.loadSamples.bind(this);
 
 		//initial state
 		this.state = {
@@ -27,6 +29,12 @@ class App extends React.Component{
 		this.setState({fishes}); //fishes =  fishes: fishes
 	}
 
+	loadSamples(){
+		this.setState({
+			fishes: sampleFishes
+		})
+	}
+
 	render(){
 		return (
 			<div className="catch-of-the-day">
@@ -34,7 +42,7 @@ class App extends React.Component{
 					<Header tagline="Fresh see food market"/>
 				</div>
 				<Order/>
-				<Inventory addFish={this.addFish}/>
+				<Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
 			</div>
 		);
 	}
