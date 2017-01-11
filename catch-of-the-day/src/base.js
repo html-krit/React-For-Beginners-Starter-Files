e33b51ca78366bd282351671d512617061bccb2f
Misha Krit <misha.krit@bitgamelabs.com>
2017-01-11 12:49:33 +0200

    firebase and components lifecycle

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import  Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyD8gO8azYuH5Rogt7hgHAmd4tHDaQu6Zm4",
+	authDomain: "catch-of-the-day-51a58.firebaseapp.com",
+	databaseURL: "https://catch-of-the-day-51a58.firebaseio.com"
+});
+
+export default base;
